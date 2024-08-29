c2050e424256922f31746888860c85b902e49e3a
Yashu Ranparia <yashuranparia136@gmail.com>
2024-08-29 18:10:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ let interval_num = 1
 
 // app.use(cors({ origin: '*' }))
 app.use(cors({
-  origin: ['https://b1ed9f26.cloudexpress-frontend-3ck.pages.dev' , 'https://908fa98a.cloudexpress-player.pages.dev'] // Replace with your allowed origins
+  origin: ['https://5ae3fd7c.cloudexpress-frontend-3ck.pages.dev' , 'https://908fa98a.cloudexpress-player.pages.dev'] // Replace with your allowed origins
 }));
 
 // app.use(bodyParser.json());
